8a04724487a1b51cd31a561211545e219d29b28a
Omkar Nale <naleomkar88@gmail.com>
2023-01-24 11:53:38 +0530

    functionality added

--- app/src/main/java/com/example/greetings_app/MainActivity.java
+++ app/src/main/java/com/example/greetings_app/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.greetings_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    TextView txt;
+    EditText edttxt;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt=findViewById(R.id.textView1);
+        edttxt=findViewById(R.id.nametext);
+        btn=findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello, "+txt, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
